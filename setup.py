f9cef33652d52148cf08d67996b87f9be5dcaeeb
Björn Edström <be@bjrn.se>
2012-10-03 21:45:14 +0200

    setup.py: More metadata.

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from distutils.core import setup, Extension
 
 _sha3 = Extension('_sha3',
@@ -9,6 +11,9 @@ _sha3 = Extension('_sha3',
 
 setup(name='_sha3',
       version='0.1',
-      description='SHA-3',
+      description='SHA-3 implementation for Python',
+      author=u'Björn Edström',
+      author_email='be@bjrn.se',
+      url='https://github.com/bjornedstrom/python-sha3',
       ext_modules=[_sha3],
       packages=['sha3'])
